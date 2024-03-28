7619184d5e88f5b3a60843ca7050c60d4a01e68b
Ali Tamer Kara <alitamerkara1907@gmail.com>
2024-03-28 14:37:19 +0300

    i upadted courseForm

--- course/src/components/CourseForm.jsx
+++ course/src/components/CourseForm.jsx
@@ -1,7 +1,39 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const CourseForm = () => {
-  return <div>CourseForm</div>;
+  const { name, description, cost } = useSelector((state) => {
+    return {
+      name: state.form.name,
+      description: state.form.description,
+      cost: state.form.cost,
+    };
+  });
+
+  return (
+    <div className="courseForm panel">
+      <h4 className="subtitle is-3">Kurs Ekle</h4>
+      <form>
+        <div className="field-group">
+          <div className="field">
+            <label className="label">Ad</label>
+            <input className="input is-expanded" />
+          </div>
+          <div className="field">
+            <label className="label">Açıklama</label>
+            <textarea className="input is-expanded" />
+          </div>
+          <div className="field">
+            <label className="label">Fiyat</label>
+            <input className="input is-expanded" type="number" />
+          </div>
+        </div>
+        <div className="field">
+          <button className="button is-primary">Kaydet</button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default CourseForm;
